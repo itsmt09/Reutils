c9efb805642afc01a0f19774c24711b1510f0c75
Manish Tekam <manish.tekam.@tejgyan.org>
2024-03-20 22:17:37 +0530

    updated todo

--- src/components/todo.jsx
+++ src/components/todo.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useState } from "react";
-export default function Todo() {
+export default function App() {
   const [items, setItems] = useState([]);
   const [title, setTitle] = useState("");
   function handleTitleAddition(e) {
@@ -10,17 +10,35 @@ export default function Todo() {
     setItems((i) => [...i, title]);
     setTitle("");
   }
-  function handleRemoveItems(item) {
-    setItems((prevItems) => prevItems.filter((i) => i !== item));
+  function handleRemoveItems(idx) {
+    setItems((prevItems) => prevItems.filter((_, id) => id !== idx));
+    console.log(items);
+  }
+  function handleRenderItem(e, idx) {
+    e.preventDefault();
+    const item = items[idx];
+    setTitle(item);
+  }
+  function handleEditItem(e, idx) {
+    e.preventDefault();
+    const item = items[idx];
+    let newItem = prompt(`update ${item}`, item);
+    setItems((prevItems) =>
+      prevItems.map((item, id) => (id === idx ? newItem : item))
+    );
+  }
+  function handleUpdateButton(idx) {
+    setItems((prevItems) =>
+      prevItems.map((item, id) => (id === idx ? title : item))
+    );
     console.log(items);
   }
   function handleUpdateItem(idx) {
     const newItems = [...items];
-    const item = newItems[idx];
-    let newItem = prompt(`update ${item}`, item);
+    const newItem = prompt(`Update this item`);
     if (newItems.indexOf(newItem) !== -1) {
       alert("Item Already Exists");
-      console.log(items);
+      console.log(`inside if :- ${items}`);
     } else {
       newItems.splice(idx, 1, newItem);
       setItems(newItems);
@@ -33,19 +51,22 @@ export default function Todo() {
       <h2>Start editing to see some magic happen!</h2>
       <div>
         <input
+          id="input"
           type="text"
           placeholder="Add a Note Title"
           value={title}
           onChange={handleTitleAddition}></input>
         <button onClick={handleAddItems}>ADD</button>
+        <button onClick={() => console.log(items)}>Console</button>
       </div>
       <ul>
         {items.map((item, idx) => (
-          <div>
-            <li key={idx}>{item}</li>
-            <button onClick={() => handleRemoveItems(item)}>Remove</button>
-            <button onClick={() => handleUpdateItem(idx)}>Edit</button>
-          </div>
+          <li key={idx}>
+            {item}&nbsp;
+            <button onClick={() => handleRemoveItems(idx)}>Remove</button>
+            <button onClick={(e) => handleRenderItem(e, idx)}>Edit</button>
+            <button onClick={() => handleUpdateButton(idx)}>Update</button>
+          </li>
         ))}
       </ul>
     </div>
